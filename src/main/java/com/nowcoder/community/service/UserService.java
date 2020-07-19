e8b08578e3b7f4f9ed43df415facd0f441b4c833
yewenxuan <619287025@qq.com>
2020-07-19 16:16:28 +0800

    开发到修改密码环节

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -1,17 +1,176 @@
 package com.nowcoder.community.service;
 
+import com.mysql.cj.exceptions.ClosedOnExpiredPasswordException;
+import com.mysql.cj.log.Log;
+import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.ognl.ObjectElementsAccessor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnJava;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.text.CompactNumberFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Service
-public class UserService {
+public class UserService implements CommunityConstant {
     @Autowired(required = false)
      private UserMapper userMapper;
+    @Autowired
+    private MailClient mailClient;
+    @Autowired
+    private TemplateEngine templateEngine;
+    @Autowired(required = false)
+    private LoginTicketMapper loginTicketMapper;
+    @Value("${community.path.domain}")
+    private String domain;
+    @Value("${server.servlet.context-path}")//注入值
+    private String contextPath;
 
     public User findUserById(int id){
+
         return userMapper.selectById(id);
+    }
+    public Map<String, Object> register(User user){
+        Map<String,Object> map=new HashMap<>();
+        //空值处理，
+        if(user==null){
+            throw new IllegalArgumentException("参数不能为空");
+        }
+        if(StringUtils.isBlank(user.getUsername())){
+            map.put("usernameMsg","账号不能为空");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getPassword())){
+            map.put("passwordMsg","密码不能为空");
+            return map;
+        }
+        if(StringUtils.isBlank(user.getEmail())){
+            map.put("emailMsg","邮箱不能为空");
+            return map;
+        }
+        //验证账号
+        User u=userMapper.selectByName(user.getUsername());
+        if(u!=null)
+        {
+            map.put("usernameMsg","该账号已存在");
+            return map;
+        }
+        //验证邮箱
+        u=userMapper.selectByEmail(user.getEmail());
+        if(u!=null){
+            map.put("emailMsg","该邮箱已被注册");
+            return map;
+        }
+        //注册用户
+        user.setSalt(CommunityUtil.generateUuid().substring(0,5));
+        user.setPassword(CommunityUtil.md5(user.getPassword()+user.getSalt()));
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUuid());
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png",new Random().nextInt(1000)));
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        //激活邮件
+        Context context=new Context();
+        context.setVariable("email",user.getEmail());
+        //
+        String url=domain+contextPath+"/activation/"+user.getId()+"/"+user.getActivationCode();
+        context.setVariable("url",url);
+        String content=templateEngine.process("/mail/activation",context);
+        mailClient.sendMail(user.getEmail(),"激活账号",content);
+
+        return map;
+
+
+
+
+    }
+    //激活账号
+    public int activation(int userId,String code){
+        User user=userMapper.selectById(userId);
+        if(user.getStatus()==1){
+            return ACTIVATION_REPEAT;
+        }
+        else if(user.getActivationCode().equals(code)){
+            userMapper.updateStatus(userId,1);
+            return ACTIVATION_SUCCESS;
+        }
+        else{
+            return ACTIVATION_FAILURE;
+        }
+
+    }
+    //登录凭证
+    public Map<String, Object>login(String username,String password,int expiredSeconds){
+        Map<String,Object>map=new HashMap<>();
+        //空值处理
+        if(StringUtils.isBlank(username)){
+            map.put("usernameMsg","账号不为空");
+            return map;
+        }
+        if(StringUtils.isBlank(password)){
+            map.put("passwordMsg","密码不能为空");
+            return map;
+        }
+
+        //验证账号
+        User user=userMapper.selectByName(username);
+        if(user==null){
+                map.put("usernameMsg","账号不存在");
+                return map;
+        }
+        //验证状态
+        if(user.getStatus()==0){
+            map.put("usernameMsg","账号未激活");
+            return map;
+        }
+        //验证密码
+        password= CommunityUtil.md5(password+user.getSalt());
+        if(!password.equals(user.getPassword())){
+            map.put("passwordMsg","密码错误");
+            return map;
+        }
+        //生成登录凭证
+        LoginTicket loginTicket=new LoginTicket();
+        loginTicket.setUserId(user.getId());
+        loginTicket.setTicket(CommunityUtil.generateUuid());
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis()+expiredSeconds*1000));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+        map.put("ticket",loginTicket.getTicket());
+        return map;
+    }
+    //退出
+    public void logout(String ticket){
+        loginTicketMapper.updateStatus(ticket,1);
+
+
+    }
+    //查询凭证
+    public LoginTicket findLoginTicket(String ticket){
+        return loginTicketMapper.selectByTicket(ticket);
+    }
+    //更新修改头像
+    public int updateHeader(int userId,String headerUrl){
+        return userMapper.updateHeader(userId,headerUrl);
+    }
+    //更新修改密码
+    public int updatePassword(int userId,String password){
+        return userMapper.updateHeader(userId,password);
     }
 
 }
--- src/test/java/com/nowcoder/community/MapperTests.java
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -1,13 +1,20 @@
 package com.nowcoder.community;
 
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.mysql.cj.log.Log;
 import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.ui.Model;
 
 import java.util.Date;
 import java.util.List;
@@ -79,6 +86,37 @@ public class MapperTests {
         }
         int rows=discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
+    }
+    @Autowired(required = false)
+    private LoginTicketMapper loginTicketMapper;
+    @Test
+    public void testInsertLoginTicket(){
+        LoginTicket loginTicket=new LoginTicket();
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis()+1000*60*10));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+    @Test
+    public void testSelectLoginTicket(){
+        LoginTicket loginTicket=loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+        loginTicketMapper.updateStatus("abc",1);
+
+        loginTicket=loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+    }
+    @Autowired
+    private HostHolder hostHolder;
+    @Test
+    public void testChangePassword(){
+        User user=userMapper.selectByName("akomwins");
+        String oldPassword="akomwins"+user.getSalt();
+        oldPassword=CommunityUtil.md5(oldPassword);
+        System.out.println(oldPassword);
+        System.out.println(user.getPassword());
+        System.out.println(oldPassword.equals(user.getPassword()));
     }
 
 }
